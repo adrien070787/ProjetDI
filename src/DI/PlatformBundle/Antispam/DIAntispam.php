869305e605598f0b3ef168aa9b5679c027327c1f
adrien070787 <adrien.rebibo@gmail.com>
2018-02-07 14:29:27 +0200

    creation du service Antispam et de son tag AntispamExtension pour Twig

---
+++ src/DI/PlatformBundle/Antispam/DIAntispam.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathanaelmrejen
+ * Date: 07/02/2018
+ * Time: 11:30
+ */
+
+namespace DI\PlatformBundle\Antispam;
+
+
+class DIAntispam
+{
+
+}
---
+++ src/DI/PlatformBundle/Twig/AntispamExtension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathanaelmrejen
+ * Date: 07/02/2018
+ * Time: 12:08
+ */
+
+namespace DI\PlatformBundle\Twig;
+
+
+class AntispamExtension
+{
+
+}
